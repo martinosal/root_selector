32a08de39a8a6a674ef07554569400f468df29e7
martinosal <martinosalomone@gmail.com>
2020-03-24 18:49:06 +0100

    merged

--- list_files.C
+++ list_files.C
@@ -1,5 +1,14 @@
 void list_files(TChain *f)
 {
-    f->Add("input_files/flav_Akt4EMTo_13022127_taggers.root");
-//    f->Add("/eos/user/m/mcentonz/Files/FTPF/FTPF_output/");
+  //    f->Add("input_files/flav_Akt4EMTo_13022127_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000001_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000002_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000003_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000004_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000005_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000006_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000007_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000030_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000079_taggers.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/flav_Akt4EMTo_13022127_000085_taggers.root");
 }
