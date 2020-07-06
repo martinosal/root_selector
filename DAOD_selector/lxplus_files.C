102bd7b11bf7b6401449adb51b2c8267844021f5
martinosal <martinosalomone@gmail.com>
2020-07-06 19:09:26 +0200

    lxplus file

--- DAOD_selector/lxplus_files.C
+++ DAOD_selector/lxplus_files.C
@@ -1,19 +1,23 @@
 void lxplus_files(TChain *f)
 {
 
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/flav_Akt4EMPf_retagT_fix.root");
+//    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/flav_Akt4EMPf_retagT_fix.root");
 //    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/flav_Akt4EMPf_BTagging201903_retagF_fix.root");
 //    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/flav_AktVR30Rmax4Rmin02Tr_BTagging201903.root");
-/*
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000001_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000002_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000003_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000004_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000005_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000006_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000007_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000030_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000079_taggers.root");
-    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/AOD_output/flav_Akt4EMTo_13022127_000085_taggers.root");
-    */
+
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_1.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_2.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_3.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_4.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_5.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_6.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_7.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_8.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_9.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_10.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_11.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_12.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_13.root");
+    f->Add("/eos/user/m/mcentonz/File/FTPF/FTPF_taggers_output/DAOD_output/DAOD_out_Akt4EMP_201903_retagF/flav_Akt4EMPf_BTagging201903_14.root");
+
 }
